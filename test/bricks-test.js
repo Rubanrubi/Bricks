12b3703457f95696791d29bbdbc3b6c5ccd43fb7
Rubanrubi <ruban@sparkouttech.com>
2021-11-19 19:16:37 +0530

    unit testing multiple scenarios

--- test/bricks-test.js
+++ test/bricks-test.js
@@ -1,6 +1,5 @@
-const chai = require('chai')
-const Bricks = artifacts.require('../contracts/Bricks.sol');
-const expect = chai.expect;
+const chai = require('chai').should().assert;
+const Bricks = artifacts.require('Bricks');
 const truffleAssert = require('truffle-assertions');
 const Web3 = require('web3');
 let web3 =  new Web3('HTTP://127.0.0.1:8545');
@@ -12,7 +11,7 @@ contract('Bricks',accounts => {
     const recipient = accounts[1];
 
     beforeEach(async () => {
-        bricks = bricks.deployed();
+        bricks = await Bricks.deployed();
     })
 
     describe('Fee Section',()=>{
@@ -25,6 +24,7 @@ contract('Bricks',accounts => {
                     return ev.enableTax = enableTax;
                 })
             }catch(err){
+                // console.log('error response',err);
                 const errorMessage = "Ownable: caller is not the owner"
                 assert.equal(err.reason, errorMessage, "Fee Should be Enabled by owner");
             }
@@ -33,21 +33,20 @@ contract('Bricks',accounts => {
         it('Enable Fee from owner', async () => {
             const enableTax = true;
             let fee = await bricks.setEnableFee(enableTax,{from: sender});
-            truffleAssert.eventEmitted(fee,'EnableFee', async(ev) =>{
-                return ev.enableTax = enableTax;
-            })
+            assert.equal(fee.receipt.status, enableTax, "Fee Should be Enabled by owner");
         })
 
         it('No tax after disable fee (Including Development & Team Wallet)', async () => {
             const enableTax = false;
             const amount = 100 * 10 ** 9;
+            const txAmount = amount.toString();
             await bricks.setEnableFee(enableTax,{from: sender});
-            let transfer = await bricks.transfer(recipient,amount,{from:sender})
+            let transfer = await bricks.transfer(recipient,txAmount,{from:sender})
             truffleAssert.eventEmitted(transfer,'Transfer', async(ev) =>{
-                assert.equal(amount, ev.value, "Recipient got full amount without tax");
+                assert.equal(txAmount, ev.value, "Recipient got full amount without tax");
                 return ev.from == sender
                 && ev.to == recipient
-                && ev.value == amount;
+                && ev.value == txAmount;
             })
         })
 
@@ -55,38 +54,53 @@ contract('Bricks',accounts => {
         it('Enable fee', async () => {
             const enableTax = true;
             let fee = await bricks.setEnableFee(enableTax,{from: sender});
-            truffleAssert.eventEmitted(fee,'EnableFee', async(ev) =>{
-                return ev.enableTax = enableTax;
-            })
+            assert.equal(fee.receipt.status, enableTax, "Enabling all fee's");
         })
 
-        it('Tax applies if fee enabled (Including Development & Team Wallet)',() => {
+        it('Tax applies if fee enabled (Including Development & Team Wallet)', async () => {
             const enableTax = true;
             let beforeTxBalance;
             let afterTxBalance;
-            const amount = 100 * 10 ** 9;
+            const amount = toString(100 * 10 ** 9);
+            const txAmount = amount.toString();
+            const expectedAmount = toString(94 * 10 ** 9);
+            const expectedTxAmount = expectedAmount.toString();
 
             await bricks.setEnableFee(enableTax,{from: sender});
 
             // Before transaction balance checking for recipient.
-            let balanceBeforeTransfer = await bricks.balanceOf(recipient);
-            truffleAssert.eventEmitted(balanceBeforeTransfer,'Balance', async(ev) =>{
-                beforeTxBalance = ev.account;
-            })
+            beforeTxBalance = await bricks.balanceOf(recipient);
 
-            let transfer = await bricks.transfer(recipient,amount,{from:sender})
+            let transfer = await bricks.transfer(recipient,txAmount,{from:sender})
             truffleAssert.eventEmitted(transfer,'Transfer', async(ev) =>{
-                assert.equal(amount, ev.value, "Recipient got amount with tax");
-                return ev.from == sender
-                && ev.to == recipient
-                && ev.value == 94 * 10 ** 9;
+                // return ev.from == sender
+                // && ev.to == recipient
+                // && ev.value == expectedTxAmount;
+                assert.equal(expectedTxAmount, ev.value, "Recipient got amount with tax");
             })
 
             // After transaction recipient account should take redistribution,development,team taxes.
-            let balanceAfterTransfer = await bricks.balanceOf(recipient);
-            truffleAssert.eventEmitted(balanceAfterTransfer,'Balance', async(ev) =>{
-                afterTxBalance = ev.rAmount;
+            afterTxBalance = await bricks.balanceOf(recipient);
+            assert.isAbove(afterTxBalance,beforeTxBalance,'Is Balance is updating or not');
+        })
+
+        it('Balance Updating while new transaction', async () => {
+            let beforeTxBalance;
+            let afterTxBalance;
+            const amount = 100 * 10 ** 9;
+            const txAmount = amount.toString();
+            // Before transaction balance checking for recipient.
+            beforeTxBalance = await bricks.balanceOf(recipient);
+
+            // Transfer
+            let transfer = await bricks.transfer(recipient,txAmount,{from:sender})
+            truffleAssert.eventEmitted(transfer,'Transfer', async(ev) =>{
+                assert.equal(txAmount, ev.value, "Transfer Successfully");
             })
+
+             // After transaction recipient account should take redistribution,development,team taxes.
+             afterTxBalance = await bricks.balanceOf(recipient);
+             assert.isAtLeast(afterTxBalance,beforeTxBalance,'Balance Increase while transfer');
         })
 
     })
@@ -96,8 +110,8 @@ contract('Bricks',accounts => {
         it('Enable/Disable Antiwhale from non owner -fails', async () => {
             const antiWhale = true;
             try{
-                let fee = await bricks.setAntiwale(antiWhale,{from: recipient});
-                truffleAssert.eventEmitted(fee,'AntiWhale', async(ev) =>{
+                let antiwale = await bricks.setAntiwale(antiWhale,{from: recipient});
+                truffleAssert.eventEmitted(antiwale,'Enable antiWhale from non-owner ', async(ev) =>{
                     return ev.enableAntiwale = antiWhale;
                 })
             }catch(err){
@@ -106,16 +120,44 @@ contract('Bricks',accounts => {
             }
         })
 
-        it('Enable/Disable Antiwhale from owner',() => {
+        it('Enable/Disable Antiwhale from owner', async () => {
+            const antiWhale = true;
+            let fee = await bricks.setAntiwale(antiWhale,{from: sender});
+            assert.equal(fee.receipt.status, antiWhale, "Enabling anti whale by owner");
 
         })
 
-        it('If AntiWhale enable need to restrict bulk transfer limit as 20000000',() => {
-
+        it('If AntiWhale enable need to restrict bulk transfer limit as 20000000', async () => {
+            const antiWhale = true;
+            const transferAmount = 20000000 * 10 ** 9;
+            const txAmount = transferAmount.toString();
+            try{
+              // Transfer
+              let transfer = await bricks.transfer(recipient,txAmount,{from:sender})
+              truffleAssert.eventEmitted(transfer,'Transfer', async(ev) =>{
+                return ev.from == sender
+                && ev.to == recipient
+                && ev.value == txAmount;
+              })
+            }catch(err){
+                const errorMessage = "Transfer amount should not be greater than 20000000";
+                assert.equal(err.reason, errorMessage, "AntiWhale Restricts this much amount While Enable");
+            }
         })
 
-        it('if antiwale is disabled above the limit (20000000) also possible to transfer',() => {
+        it('if antiwale is disabled sender able to transfer above the limit (20000000) also possible', async () => {
 
+            const antiWhale = false;
+            const transferAmount = 20000000 * 10 ** 9;
+            const txAmount = transferAmount.toString();
+              // Transfer
+              let transfer = await bricks.transfer(recipient,txAmount,{from:sender})
+              truffleAssert.eventEmitted(transfer,'Transfer', async(ev) =>{
+                assert.equal(ev.value, txAmount, "AntiWhale does'nt restricts this much amount because disabled");
+                return ev.from == sender
+                && ev.to == recipient
+                && ev.value == txAmount;
+              })
         })
 
     })
--- truffle-config.js
+++ truffle-config.js
@@ -25,7 +25,7 @@
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const {mnemonic,BscApiKey} = require('./secrets.json');
+const {mnemonic,BscApiKey,projectId} = require('./secrets.json');
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -61,13 +61,14 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     // rinkeby: {
-    // provider: () => new HDWalletProvider(privateKey, `https://data-seed-prebsc-1-s1.binance.org:8545/`),
+    // provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${projectId}`),
     // network_id: 4,       // Ropsten's id
     // gas: 5500000,        // Ropsten has a lower block limit than mainnet
     // // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
     // // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+    // For BSC Testnet
     testnet: {
       provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,
